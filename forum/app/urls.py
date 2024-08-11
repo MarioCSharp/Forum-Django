d099a2e4f15b8a2dbbdfb44f39dc7183ab1fb4b6
Mario Petkov <mario.18@abv.bg>
2024-08-11 18:03:28 +0300

    CRUD done.

--- forum/app/urls.py
+++ forum/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.posts, name='posts'),
     path('posts/<int:id>/', views.post_detail, name='post_details'),
-    path('posts/delete/<int:id>/', views.post_delete, name='post_delete')
+    path('posts/delete/<int:id>/', views.post_delete, name='post_delete'),
+    path('posts/add/', views.post_add, name='post_add'),
+    path('posts/edit/<int:id>', views.post_edit, name='post_edit')
 ]
--- forum/app/views.py
+++ forum/app/views.py
@@ -6,18 +6,9 @@ def homepage(request):
     return render(request, 'home.html')
 
 def posts(request):
-    if request.method == 'GET':
-        allPosts = Post.objects.all()
-        form = PostForm()
-        return render(request, 'posts.html', {'posts': allPosts, 'form': form})
-    else:
-        form = PostForm(request.POST)
-        if form.is_valid():
-            new_post = form.save()
-            return redirect(f'/posts/{new_post.id}/')
-
-        allPosts = Post.objects.all()
-        return render(request, 'posts.html', {'posts': allPosts, 'form': form})
+    allPosts = Post.objects.all()
+    return render(request, 'posts.html', {'posts': allPosts})
+    
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
@@ -27,4 +18,32 @@ def post_delete(request, id):
     post = Post.objects.get(pk=id)
     post.delete()
 
-    return redirect('/posts')
+    return redirect('/')
+
+def post_add(request):
+    if request.method == 'GET':
+        form = PostForm()
+        return render(request, 'post_add.html', {'form': form})
+    elif request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            saved_form = form.save()
+
+            return redirect(f'/posts/{saved_form.id}/')
+        
+        return render(request, 'post_add.html', {'form': form})
+    
+def post_edit(request, id):
+    if request.method == 'GET':
+        post = Post.objects.get(pk=id)
+        form = PostForm(instance=post)
+
+        return render(request, 'post_edit.html', {'form': form})
+    elif request.method == 'POST':
+        post = Post.objects.get(pk=id)
+        edited = PostForm(request.POST, instance=post)
+        if edited.is_valid():
+            saved = edited.save()
+            return redirect(f'/posts/{saved.id}/')
+        
+        return render(request, 'post_edit.html', {'form': edited})
