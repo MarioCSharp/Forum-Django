959b07d17e271c67849b7304c1b6ce35700e3c10
Mario Petkov <mario.18@abv.bg>
2024-08-11 13:24:48 +0300

    CRUD with django.

--- forum/app/urls.py
+++ forum/app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name="home"),
-    path("posts/", views.posts, name='posts'),
-    path('posts/<int:id>/', views.post_detail, name='post_details')
+    path("", views.posts, name='posts'),
+    path('posts/<int:id>/', views.post_detail, name='post_details'),
+    path('posts/delete/<int:id>/', views.post_delete, name='post_delete')
 ]
--- forum/app/views.py
+++ forum/app/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def homepage(request):
     return render(request, 'home.html')
 
 def posts(request):
-    allPosts = Post.objects.all()
-    return render(request, 'posts.html', {'posts': allPosts})
+    if request.method == 'GET':
+        allPosts = Post.objects.all()
+        form = PostForm()
+        return render(request, 'posts.html', {'posts': allPosts, 'form': form})
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save()
+            return redirect(f'/posts/{new_post.id}/')
+
+        allPosts = Post.objects.all()
+        return render(request, 'posts.html', {'posts': allPosts, 'form': form})
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'post_details.html', {'post': post})
+
+def post_delete(request, id):
+    post = Post.objects.get(pk=id)
+    post.delete()
+
+    return redirect('/posts')
